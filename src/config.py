272b50a21b0fc5ac863de8c15b6732d4d93c7ae6
Jack <tfermin1827@hotmail.com>
2020-04-02 22:27:03 -0400

    Added program compiler

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@
 Edit this accordingly.
 """
 
-icue_dll_path = "./dll/CUESDK.x64_2017.dll"
+icue_dll_path = "./dist/main/dll/CUESDK.x64_2017.dll"
 wait_for_change = False  # Wait's for screen changes to update colors
 smooth = False  # Smooth color transition
 screen = -1  # Last Screen
