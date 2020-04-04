281f7657e24789d4fbdffb2efccc6ff0b816d1bb
Jack <tfermin1827@hotmail.com>
2020-04-04 15:54:37 -0400

    Mode: Screen reactive

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from src.config import icue_dll_path
+from src.handlers.modes.screen import ScreenReact
+from src.handlers.sdk.icue import iCue
+
+
 if __name__ == '__main__':
     print("Program initiated!")
+    icue_sdk = iCue(icue_dll_path)
+    screen_mode = ScreenReact()
+    screen_mode.run(icue_sdk)
