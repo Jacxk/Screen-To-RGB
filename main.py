6b317a5d42797fdd581a2d3e7e31cae606b3a3a3
Jack <tfermin1827@hotmail.com>
2020-08-01 22:20:26 -0400

    Started sound mode

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.config import icue_dll_path
 from src.handlers.modes.keyboard import KeyboardPress
 from src.handlers.modes.screen import ScreenReact
+from src.handlers.modes.sound import SoundReact
 from src.handlers.sdk.icue import iCue
 
 if __name__ == '__main__':
@@ -8,7 +9,9 @@ if __name__ == '__main__':
     icue_sdk = iCue(icue_dll_path)
 
     if icue_sdk.available():
-        screen_mode = ScreenReact(sdks=[icue_sdk])
-        keyboard = KeyboardPress(sdks=[icue_sdk])
-        keyboard.start()
-        screen_mode.start()
+        #screen_mode = ScreenReact(sdks=[icue_sdk])
+        #keyboard = KeyboardPress(sdks=[icue_sdk])
+        sound = SoundReact(sdks=[icue_sdk])
+        sound.start()
+        #keyboard.start()
+        #screen_mode.start()
---
+++ src/handlers/modes/sound.py
@@ -0,0 +1,24 @@
+from src.interface.mode import Mode
+import sounddevice as sd
+import numpy as np
+
+
+class SoundReact(Mode):
+    def __init__(self, **kwargs):
+        super().__init__("Sound Reactive", **kwargs)
+
+    def run(self):
+        super().run()
+
+        def print_sound(indata, frames, time, status):
+            volume_norm = np.linalg.norm(indata) * 10
+            print("|" * int(volume_norm))
+
+        with sd.InputStream(callback=print_sound):
+            sd.sleep(10000)
+
+        while self.active:
+            pass
+
+    def disable(self):
+        super().disable()
