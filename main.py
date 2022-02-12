1e43c773eee23cf247b05d4446bc444dd7974f44
Jack <32645451+Jacxk@users.noreply.github.com>
2022-02-11 20:14:25 -0500

    feat:Added an exit handler to disable all modes

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from src.interface.mode import available_modes
 from src.handlers.modes import screen, keyboard
 from src.handlers.sdk.icue import iCue
+import signal
+
+
+def on_exit(*args):
+    print('\nShutting down program.')
+    for mode in available_modes:
+        mode.disable()
+    pass
+
+signal.signal(signal.SIGINT, on_exit)
+signal.signal(signal.SIGTERM, on_exit)
+    
 
 if __name__ == '__main__':
     print("Program initiated!")
